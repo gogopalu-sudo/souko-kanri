14f2ec30089715d732ad167bf44a1df3043b7772
gogopalu-sudo <gogopalu@gmail.com>
2026-06-16 23:11:05 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'wh-cache-v1';
+
+// インストール時：アプリをキャッシュ
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./'])));
+  self.skipWaiting();
+});
+
+// 古いキャッシュを削除
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// ネットワーク優先・オフライン時はキャッシュから
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
